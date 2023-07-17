7c6472e17f6da7557d57019ae2ee97a290955b56
agusbogado97 <agus.y.roger@gmail.com>
2023-07-16 22:37:59 -0300

    Primeras ideas

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/* const anioActual = 2023;
+
+class Auto {
+    constructor(marca, color, motor, precio, modelo){
+        this.marca = marca;
+        this.color = color;
+        this.motor = motor;
+        this.precio = precio;
+        this.modelo = modelo;
+    }
+}
+
+const autoPromo1 = new Auto("Ford", "Rojo", 1.4, 1500000, 1997);
+
+console.log(autoPromo1.color);
+
+console.log(anioActual)
+ */
+
+
